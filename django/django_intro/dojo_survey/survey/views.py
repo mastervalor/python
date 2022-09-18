ab2148a916dfeae3f42afd7c206c3c60cebba59f
Mourad Marzouk <101042117+mastervalor@users.noreply.github.com>
2022-09-17 21:29:45 -0700

    Django post assigment

--- django/django_intro/dojo_survey/survey/views.py
+++ django/django_intro/dojo_survey/survey/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def results(request):
+    context = {
+        'name': request.POST['name'],
+        'location': request.POST['location'],
+        'language': request.POST['language'],
+        'comment': request.POST['comment']
+    }
+    return render(request, 'result.html', context)
